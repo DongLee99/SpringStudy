354518c50c5b91591c26619c6bbc92cf845229c6
DongLee99 <jhnj741@naver.com>
2021-01-11 22:22:49 +0900

    Chapter3 2021/01/11 save와 findAll 테스트

---
+++ src/test/java/com/DongLee99/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.DongLee99.book.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                    .title(title)
+                    .content(content)
+                    .author("jhnj741@gmail.com")
+                    .build()); // 위 의 값을 save 를 통해 저장하
+
+        //when
+        List<Posts> postsList = postsRepository.findAll(); // 모든것을 찾아온
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
