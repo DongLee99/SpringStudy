420967bb9e943ba43d19693980cab7ecd9133330
DongLee99 <jhnj741@naver.com>
2021-01-16 15:42:47 +0900

    Chapter4 2021/01/16 CH4 머스테치로 화면 구성하기 수정, 삭제

--- src/main/java/com/DongLee99/book/springboot/web/IndexController.java
+++ src/main/java/com/DongLee99/book/springboot/web/IndexController.java
@@ -1,11 +1,14 @@
 package com.DongLee99.book.springboot.web;
 
 import com.DongLee99.book.springboot.service.PostsService;
+import com.DongLee99.book.springboot.web.dto.PostsResponseDto;
 import com.DongLee99.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -19,5 +22,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto); // mustache 를 가보면 posts 가 아닌 post
+        return "posts-update";
     }
 }
